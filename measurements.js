a0ade981e1e4b04c6e787fdae561e57f70f39bee
sricharan001 <115539612+sricharan001@users.noreply.github.com>
2024-05-06 10:56:57 -0500

    Update measurements.js

--- measurements.js
+++ measurements.js
@@ -11,11 +11,6 @@
     style.appendChild(document.createTextNode(css));
     }
 }
-var reactAppContainer = document.createElement('div');
-reactAppContainer.id = 'root2';
-reactAppContainer.style.overflow = 'scroll';
-reactAppContainer.style.width = '50%';
-document.body.appendChild(reactAppContainer);
 
 
 document.head.innerHTML += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />'
